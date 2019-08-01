ac5e1224538f889e36f22fc40cfba1bb1e4a9a3a
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-08-01 22:45:46 +0200

    feat: getting a list of projects from github

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,58 @@
 "use strict";
 
-// service worker registration - remove if you're not going to use it
+const boxProjects = document.querySelector('.wrapper-cards');
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
+fetch("https://api.github.com/users/Andrzej-Jablonski-project/repos?sort=desc")
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      const {
+        name,
+        description,
+        html_url,
+        homepage
+      } = repo;
+      if (description) {
+        const section = document.createElement('section');
+        const icon = document.createElement('img');
+        const projectName = document.createElement('h3');
+        const desc = document.createElement('p');
+        const footer = document.createElement('footer');
+        const code = document.createElement('a');
+        const demo = document.createElement('a');
 
-// place your code below
+        boxProjects.appendChild(section);
+        section.appendChild(icon);
+        section.appendChild(projectName);
+        section.appendChild(desc);
+        section.appendChild(footer);
+        footer.appendChild(demo);
+        footer.appendChild(code);
 
+        section.classList.add('card');
+        icon.classList.add('card__icon');
+        projectName.classList.add('card__project-name');
+        desc.classList.add('card__project-description');
+        footer.classList.add('card__footer');
+        demo.classList.add('card__link');
+        code.classList.add('card__link', 'card__link--github');
 
-console.log(`Hello world!`);
+        icon.setAttribute("src", "../assets/img/github-icon.svg");
+        demo.setAttribute('href', homepage, );
+        demo.setAttribute('target', '_blank');
+        demo.setAttribute('rel', 'noopener');
+        code.setAttribute('href', html_url);
+        code.setAttribute('target', '_blank');
+        code.setAttribute('rel', 'noopener');
 
-
+        projectName.textContent = name;
+        desc.textContent = description;
+        demo.textContent = 'Demo';
+        code.textContent = 'Github';
+      }
+    }
+  })
+  .catch(error => {
+    console.log(error)
+  })

fc9cb1dc81e8ebbfc75937ca2172aefa7e76c71f
Andrzej Jabłoński <andrzej@audioszum.pl>
2019-08-07 15:02:53 +0200

    chore: refactoring the code, styled for tablet screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const boxProjects = document.querySelector('.wrapper-cards');
+const boxProjects = document.querySelector('.card-list');
 
 fetch("https://api.github.com/users/Andrzej-Jablonski-project/repos?sort=desc")
   .then(resp => resp.json())
@@ -14,42 +14,20 @@ fetch("https://api.github.com/users/Andrzej-Jablonski-project/repos?sort=desc")
         homepage
       } = repo;
       if (description) {
-        const section = document.createElement('section');
-        const icon = document.createElement('img');
-        const projectName = document.createElement('h3');
-        const desc = document.createElement('p');
-        const footer = document.createElement('footer');
-        const code = document.createElement('a');
-        const demo = document.createElement('a');
-
-        boxProjects.appendChild(section);
-        section.appendChild(icon);
-        section.appendChild(projectName);
-        section.appendChild(desc);
-        section.appendChild(footer);
-        footer.appendChild(demo);
-        footer.appendChild(code);
-
-        section.classList.add('card');
-        icon.classList.add('card__icon');
-        projectName.classList.add('card__project-name');
-        desc.classList.add('card__project-description');
-        footer.classList.add('card__footer');
-        demo.classList.add('card__link');
-        code.classList.add('card__link', 'card__link--github');
-
-        icon.setAttribute("src", "../assets/img/github-icon.svg");
-        demo.setAttribute('href', homepage, );
-        demo.setAttribute('target', '_blank');
-        demo.setAttribute('rel', 'noopener');
-        code.setAttribute('href', html_url);
-        code.setAttribute('target', '_blank');
-        code.setAttribute('rel', 'noopener');
-
-        projectName.textContent = name;
-        desc.textContent = description;
-        demo.textContent = 'Demo';
-        code.textContent = 'Github';
+        boxProjects.innerHTML += ` 
+        <li class="card">
+            <div class="card__container">
+              <img src="../assets/img/github-icon.svg" alt="" class="card__icon">
+              <h3 class="card__project-name">${name}</h3>
+              <p class="card__project-description">${description}</p>
+            </div>
+            <footer class="card__footer">
+          ${
+            homepage ? `<a href="${homepage}" target="_blank" rel="nofollow noreferrer" class="card__link">Demo</a>` : ''
+          }
+              <a href="${html_url}" target="_blank" rel="nofollow noreferrer" class="card__link card__link--github">Github</a>
+            </footer>
+        </li>`;
       }
     }
   })
